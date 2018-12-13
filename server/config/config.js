dcda6283e79d94f2099604ad5bc778a79ec03af4
Jose A Lopez <josearleylopez@gmail.com>
2018-12-13 14:21:51 -0500

    Variable entorno URI Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://jall:jall1319@ds249605.mlab.com:49605/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
