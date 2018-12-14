e25ccbfc07cef126bdfaf3e154198eb3da811427
Jose A Lopez <josearleylopez@gmail.com>
2018-12-14 07:50:15 -0500

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,10 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo';
 
 
+// *****CLIENT_ID GOOGLE*****
+process.env.CLIENT_ID = process.env.CLIENT_ID || '730164162197-695ge5asfp8vvt4gpjagmh6gf7hf8iht.apps.googleusercontent.com';
+
+
 // *****BASE DE DATOS*****
 let urlDB;
 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const path = require('path');
+
 
 
 
@@ -13,6 +15,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
+// console.log(path.resolve(__dirname, '../public'));
 
 // Configuracion global del rutas
 app.use(require('./routes/index'));
