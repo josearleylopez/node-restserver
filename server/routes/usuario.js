4b5e45019658c5fa1f79d5d78670d89880e0958a
Jose A Lopez <josearleylopez@gmail.com>
2018-12-13 20:58:59 -0500

    Test actualizacion usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -37,7 +37,7 @@ app.get('/usuario', verificaToken, (req, res) => {
                 })
             })
         });
-})
+});
 
 app.post('/usuario', [verificaToken, verificaRoleAdmin], (req, res) => {
     let body = req.body;
@@ -65,7 +65,7 @@ app.post('/usuario', [verificaToken, verificaRoleAdmin], (req, res) => {
 
 app.put('/usuario/:id', [verificaToken, verificaRoleAdmin], (req, res) => {
     let id = req.params.id;
-    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+    let body = _.pick(req.body, ['nombre', 'img', 'role', 'estado']);
     Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
         if (err) {
             return res.status(400).json({
